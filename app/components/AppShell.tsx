e3ce33c9dd37a2202919ebf5268231f8b6c17bc1
rishii-sudo <rishii-sudo@users.noreply.github.com>
2026-09-04 19:32:27 +0530

    updated login page

--- app/components/AppShell.tsx
+++ app/components/AppShell.tsx
@@ -12,6 +12,7 @@ export default function AppShell({
   const pathname = usePathname();
 
   const isStandaloneRoute =
+    pathname === "/" ||
     pathname === "/login" ||
     pathname.startsWith("/patient") ||
     pathname.startsWith("/doctor/login") ||
